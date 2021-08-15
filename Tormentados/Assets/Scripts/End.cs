ca7f4a87a74d6389f53c21cdc2ede53ce9a8f760
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2021-08-15 12:11:59 -0300

    game-commit

---
+++ Tormentados/Assets/Scripts/End.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public Save_Team saveTeam;
+    public GameObject[] listChars;
+    public void Start()
+    {
+        saveTeam = GameObject.Find("SaveTeam").GetComponent<Save_Team>();
+        ShowTeam();
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ShowTeam()
+    {
+        for(int i = 0; i < saveTeam.characters.Length; i++)
+        {
+            if(saveTeam.characters[i] == 1)
+                listChars[i].SetActive(true);
+        }
+    } 
+}
